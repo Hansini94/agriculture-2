248fec26b4335f930d9f681bd530dbd1a93072dc
Ayodhya Leitan <93565002+AyodhyaTG@users.noreply.github.com>
2022-07-04 16:19:51 +0530

    Awards - Calling application

--- application/models/adminpanel/Awards_model.php
+++ application/models/adminpanel/Awards_model.php
@@ -43,6 +43,19 @@ class Awards_model extends CI_Model {
         }
     }
 
+    public function get_calling_application_data() {
+
+        $this->db->from('tbl_calling_application');
+		$this->db->where('id', 1);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
     ###############################
 
 }
