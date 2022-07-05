e817a2757e7b9a56d99beddfd2077ca7c58221b2
Ayodhya Leitan <93565002+AyodhyaTG@users.noreply.github.com>
2022-07-05 18:31:48 +0530

    Academics - BSc.Agtech & Mgt - introduction, structure degree, core prog structure, core programe seq, adv prog structure

---
+++ application/models/adminpanel/Academic_model.php
@@ -0,0 +1,100 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Academic_model extends CI_Model {
+
+    public function get_bsc_agtech_data() {
+
+        $this->db->from('tbl_bsc_agtech_mgt');
+		$this->db->where('id', 1);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_bsc_core_programme_structure_list() {
+
+        $this->db->from('tbl_bsc_core_programme_structure');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_edit_bsc_core_programme_structure($id) {
+
+        $this->db->from('tbl_bsc_core_programme_structure');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_agtech_course_sequence_list() {
+
+        $this->db->from('tbl_agtech_course_sequence');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_edit_agtech_course_sequence($id) {
+
+        $this->db->from('tbl_agtech_course_sequence');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_advance_programme_structure_list() {
+
+        $this->db->from('tbl_agtech_adv_prog_structure');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_edit_advance_programme_structure($id) {
+
+        $this->db->from('tbl_agtech_adv_prog_structure');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    ###############################
+
+}
+
+?>
