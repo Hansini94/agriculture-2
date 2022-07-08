d1eaf9cec5ae60e4773e5a6852c0a3fc1f5e8f2a
Ayodhya Leitan <93565002+AyodhyaTG@users.noreply.github.com>
2022-07-08 16:49:28 +0530

    academics - bsc asf introduction

--- application/models/adminpanel/Academic_model.php
+++ application/models/adminpanel/Academic_model.php
@@ -170,6 +170,19 @@ class Academic_model extends CI_Model {
         }
     }
 
+    public function get_bsc_asf_introduction_data() {
+
+        $this->db->from('tbl_bsc_asf_introduction');
+		$this->db->where('id', 1);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
     ###############################
 
 }
