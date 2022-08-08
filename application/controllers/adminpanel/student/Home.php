d258bd1813c8e503c5637d85696794a120e80e2c
GHLakshani <110371471+Hansini94@users.noreply.github.com>
2022-08-08 12:25:21 +0530

    Student Life Home
    Home page for Student Life

---
+++ application/controllers/adminpanel/student/Home.php
@@ -0,0 +1,168 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/* date : 08-08-2022
+ * author : Hansini
+ */
+
+Class Home extends CI_Controller {
+
+    private $table_name = "tbl_student_home";
+    private $page_id = "245";
+    private $redirect_path = "adminpanel/student/home";
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->library('form_validation');
+        $this->load->library('common_library');
+        $this->load->helper('flexigrid');
+        $this->load->helper('ckeditor');
+        $this->load->model('adminpanel/common_model');
+        set_title("Home");
+        $user_privilages = $this->common_model->get_page_detail($this->page_id);
+        $this->session->set_userdata('u_privilages', $user_privilages);
+    }
+
+    public function index() {
+        $data['ckeditor_tContentEnglish'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContentEnglish',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+
+        $data['ckeditor_tContentSinhala'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContentSinhala',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+
+        $data['ckeditor_tContentTamil'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContentTamil',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+		
+        $data['cSaveStatus']= 'A';
+        $data['list_data'] = $this->common_model->get_all_data_list($this->table_name);
+        $this->load->view('adminpanel/header_view');
+        $this->load->view('adminpanel/student/home_view', $data);
+        $this->load->view('adminpanel/footer_view');
+    }
+
+    public function save_tab($data = '') {
+        $cSaveStatus = $this->input->post('cSaveStatus', TRUE);
+        $id = $this->input->post('id', TRUE);
+        if ($cSaveStatus === 'E') {
+            if ($this->common_model->update_saved_data($this->table_name)) {
+                //$tDes = "saved data has been updated";
+                //$this->common_model->add_log($tDes);
+                $this->session->set_flashdata('message_saved', 'Saved successfully.');
+                redirect(base_url() . 'adminpanel/student/home');
+            } else {
+                $this->session->set_flashdata('message_error', 'Save fail!');
+                redirect(base_url() . 'adminpanel/student/home');
+            }
+        } else {
+            if ($this->common_model->save_data($this->table_name)) {
+                //$tDes = "saved data has been updated";
+                //$this->common_model->add_log($tDes);
+                $this->session->set_flashdata('message_saved', 'Saved successfully.');
+                redirect(base_url() . 'adminpanel/student/home');
+            } else {
+                $this->session->set_flashdata('message_error', 'Save fail!');
+                redirect(base_url() . 'adminpanel/student/home');
+            }
+        }
+    }
+	
+	public function edit() {
+        $data['ckeditor_tContentEnglish'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContentEnglish',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+
+        $data['ckeditor_tContentSinhala'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContentSinhala',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+
+        $data['ckeditor_tContentTamil'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContentTamil',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+		
+        $data['cSaveStatus']= 'E';
+        $data['list_data'] = $this->common_model->get_all_data_list($this->table_name);
+		$recId = $this->uri->segment(5);
+		$data['edit_data'] = $this->common_model->get_edit_data($recId, $this->table_name);
+        $this->load->view('adminpanel/header_view');
+        $this->load->view('adminpanel/student/home_view', $data);
+        $this->load->view('adminpanel/footer_view');
+    }
+
+    public function change_status() {
+
+        $this->common_library->check_privilege('p_edit');
+        if ($this->common_library->check_privilege('p_edit')) {
+            $this->common_library->flexigrid_change_status($this->redirect_path, $this->table_name);
+        } else {
+            $this->session->set_flashdata('message_restricted', 'You do not have permission.');
+            redirect(base_url() . $this->redirect_path);
+        }
+    }
+
+    public function delete_record() {
+        $this->common_library->check_privilege('p_edit');
+        if ($this->common_library->check_privilege('p_delete')) {
+            $this->common_library->flexigrid_delete_record($this->redirect_path, $this->table_name);
+        } else {
+            $this->session->set_flashdata('message_restricted', 'You do not have permission.');
+            redirect(base_url() . $this->redirect_path);
+        }
+    }
+
+}
+
+?>
