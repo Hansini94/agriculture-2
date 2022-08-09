cb0e4e94d062ddf3d83490663ef0918d8df82c37
GHLakshani <110371471+Hansini94@users.noreply.github.com>
2022-08-09 09:24:40 +0530

    Current And Future Students

--- application/models/adminpanel/Student_model.php
+++ application/models/adminpanel/Student_model.php
@@ -3,10 +3,9 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Student_model extends CI_Model {
+class Student_model extends CI_Model {   
 
-    
-
+    //*******************Student Life*************************
     public function get_sports_list() {
 
         $this->db->from('tbl_student_sports');
@@ -56,11 +55,148 @@ class Student_model extends CI_Model {
             return array();
         }
     }
+    
 
+    //****************************Future Students***********************
+    public function get_future_student_data() {
 
+        $this->db->from('tbl_fs_home');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
 
+    public function get_edit_future_student($id) {
+
+        $this->db->from('tbl_fs_home');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+    
+     //****************************Current Students***********************
+     public function get_current_student_data() {
+
+        $this->db->from('tbl_cs_home');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_edit_current_student($id) {
+
+        $this->db->from('tbl_cs_home');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_prospectus_data() {
+
+        $this->db->from('tbl_prospectus');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
 
     
+    public function get_academic_calender_data() {
+
+        $this->db->from('tbl_academic_calender');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+
+    public function get_scholarship_data() {
+
+        $this->db->from('tbl_faculty_level_schol');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_video_list() {
+
+        $this->db->from('tbl_video_laboratory');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_edit_video($id) {
+
+        $this->db->from('tbl_video_laboratory');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_deans_list() {
+
+        $this->db->from('tbl_deans_list');
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    public function get_edit_deans_list($id) {
+
+        $this->db->from('tbl_deans_list');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+       // echo $this->db->last_query();exit();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
 }
 
 ?>
