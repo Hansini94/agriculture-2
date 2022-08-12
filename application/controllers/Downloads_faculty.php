00011189c459a46e0a664ebd7a7bceccc3930d68
GHLakshani <110371471+Hansini94@users.noreply.github.com>
2022-08-12 10:15:11 +0530

    Downloads front end view

---
+++ application/controllers/Downloads_faculty.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*
+ * date : 12-08-2022
+ * author : Hansini
+ */
+
+Class Downloads_faculty extends CI_Controller {
+
+    public function index() {
+
+        $this->load->model('frontend_model/downloads_model');
+
+        $data = array();
+        $data_header = array();
+
+        $data['enewsletter'] = $this->downloads_model->get_enewsletter_downloads();
+        $data['faurs'] = $this->downloads_model->get_faurs_downloads();
+        $data['hanthana'] = $this->downloads_model->get_hanthana_downloads();
+        
+        // var_dump($data);exit();
+
+        $this->load->view('frontendview/inner_header_view',$data_header);
+        $this->load->view('frontendview/downloads/faculty_downloads_view',$data);
+        $this->load->view('frontendview/footer_view', $data_header);
+       
+    }    
+    
+}
+
+?>
+
+    
