5f3b434fc13d43232e654c5b4e2232b8fda48f8f
GHLakshani <110371471+Hansini94@users.noreply.github.com>
2022-08-15 13:52:45 +0530

    Faculty and Staff
    Addition of page to admin panel

---
+++ application/controllers/adminpanel/faculty_staff/Home.php
@@ -0,0 +1,124 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/* date : 15-08-2022
+ * author : Hansini
+ */
+
+Class Home extends CI_Controller {
+
+    private $table_name = "tbl_faculty_staff_content";
+    private $page_id = "279";
+    private $redirect_path = "adminpanel/faculty_staff/home";
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->library('form_validation');
+        $this->load->library('common_library');
+        $this->load->helper('flexigrid');
+        $this->load->helper('ckeditor');
+        $this->load->model('adminpanel/common_model');
+        $this->load->model('adminpanel/faculty_staff_model');
+        set_title("Faculty and Staff");
+        $user_privilages = $this->common_model->get_page_detail($this->page_id);
+        $this->session->set_userdata('u_privilages', $user_privilages);
+    }
+
+    public function index() {	
+        
+        $data['ckeditor_tContent'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tContent',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+
+        $data['ckeditor_tSubContent'] = array(
+            //ID of the textarea that will be replaced
+            'id' => 'tSubContent',
+            'path' => 'assets/js/ckeditor',
+            //Optionnal values
+            'config' => array(
+                'toolbar' => "Full", //Using the Full toolbar
+                'width' => "100%", //Setting a custom width
+                'height' => '200px', //Setting a custom height
+            ),            
+        );
+
+        
+        $data['cSaveStatus']= 'E';
+		
+        $data['edit_date'] = $this->faculty_staff_model->get_faculty_staff_data();
+		//echo 'ff'; exit();
+        $this->load->view('adminpanel/header_view');
+        $this->load->view('adminpanel/faculty_staff/home_view', $data);
+        $this->load->view('adminpanel/footer_view');
+    }
+	
+
+    public function save_data($data = '') {
+        $cSaveStatus = $this->input->post('cSaveStatus', TRUE);
+        $id = $this->input->post('id', TRUE);
+        if ($cSaveStatus === 'E') {
+            if ($this->common_model->update_saved_data('tbl_faculty_staff_content')) {
+                //$tDes = "saved data has been updated";
+                //$this->common_model->add_log($tDes);
+                $this->session->set_flashdata('message_saved', 'Saved successfully.');
+                redirect(base_url() . 'adminpanel/faculty_staff/home');
+            } else {
+                $this->session->set_flashdata('message_error', 'Save fail!');
+                redirect(base_url() . 'adminpanel/faculty_staff/home');
+            }
+        } else {
+            if ($this->common_model->save_data('tbl_faculty_staff_content')) {
+                //$tDes = "saved data has been updated";
+                //$this->common_model->add_log($tDes);
+                $this->session->set_flashdata('message_saved', 'Saved successfully.');
+                redirect(base_url() . 'adminpanel/faculty_staff/home');
+            } else {
+                $this->session->set_flashdata('message_error', 'Save fail!');
+                redirect(base_url() . 'adminpanel/faculty_staff/home');
+            }
+        }
+    }
+
+    public function remove_image() {
+        if ($this->common_library->check_privilege('p_edit')) {
+            $imageID = $this->security->xss_clean($this->uri->segment(5));
+            $field = $this->security->xss_clean($this->uri->segment(6));
+            $path = 'front_img/';
+            $img = $this->security->xss_clean($this->uri->segment(7));
+            $this->load->model('adminpanel/common_model');
+            $postimage_delete = $this->common_model->delete_image($imageID, $field, $this->table_name, $path, $img);
+            if ($postimage_delete == TRUE) {
+                redirect(base_url() . "adminpanel/faculty_staff/home/update_details/".$imageID);
+            } else {
+                $this->session->set_flashdata('message_error', 'Delete fail!');
+                redirect(base_url() . "adminpanel/faculty_staff/home/update_details/".$imageID);
+            }
+        }
+    }
+
+    public function update_details() {
+        if ($this->common_library->check_privilege('p_edit')) {
+            $data = $this->common_library->flexigrid_update_user($this->table_name);
+            $this->index();
+        } else {
+            $this->session->set_flashdata('message_restricted', 'You do not have permission..');
+            redirect(base_url() . $this->redirect_path);
+        }
+    }
+
+
+}
+
+?>
+mm
