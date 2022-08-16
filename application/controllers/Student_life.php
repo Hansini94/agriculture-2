5172f3554cebd3fdb6a72822e992557083775596
GHLakshani <110371471+Hansini94@users.noreply.github.com>
2022-08-16 15:24:20 +0530

    Student life

--- application/controllers/Student_life.php
+++ application/controllers/Student_life.php
@@ -26,7 +26,43 @@ Class Student_life extends CI_Controller {
         $this->load->view('frontendview/inner_header_view',$data_header);
         $this->load->view('frontendview/student/student_life_view',$data);
         $this->load->view('frontendview/footer_view', $data_header);       
-    }   
+    } 
+    
+    public function ss_sinhala() {
+
+        $this->load->model('frontend_model/student_life_model');
+        $this->load->model('frontend_model/home_page_model');
+
+        $data = array();
+        $data_header = array();
+
+        $data['data'] = $this->student_life_model->get_home_content();    
+        $data_header['quick_links'] = $this->home_page_model->get_quick_list();  
+        
+        // var_dump($data);exit();
+
+        $this->load->view('frontendview/inner_header_view',$data_header);
+        $this->load->view('frontendview/student/student_life_sinhala_view',$data);
+        $this->load->view('frontendview/footer_view', $data_header);       
+    } 
+
+    public function ss_tamil() {
+
+        $this->load->model('frontend_model/student_life_model');
+        $this->load->model('frontend_model/home_page_model');
+
+        $data = array();
+        $data_header = array();
+
+        $data['data'] = $this->student_life_model->get_home_content();    
+        $data_header['quick_links'] = $this->home_page_model->get_quick_list();  
+        
+        // var_dump($data);exit();
+
+        $this->load->view('frontendview/inner_header_view',$data_header);
+        $this->load->view('frontendview/student/student_life_tamil_view',$data);
+        $this->load->view('frontendview/footer_view', $data_header);       
+    }
 
     
     
